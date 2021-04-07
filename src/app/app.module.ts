9c4970ffa0fc68d3729bc19061a9fb648d8c33b1
PavlosCl <Pablo.quijano@hotmail.cl>
2021-04-07 15:47:26 -0400

    SPA Part 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,8 +18,11 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 
-import { DishService } from './services/dish.service';
 import { AppRoutingModule } from './app-routing/app-routing.module';
+
+import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
+
 
 @NgModule({
   declarations: [
@@ -43,7 +46,7 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     MatCardModule,
     MatButtonModule,
   ],
-  providers: [DishService],
+  providers: [DishService,PromotionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
